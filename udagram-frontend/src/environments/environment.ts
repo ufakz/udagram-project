c5e9b6212593f3ca857fb6fa881868cca8170050
Farouq Umar <ufakz99@gmail.com>
2022-07-10 14:33:58 +0100

    fix: host

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab81dfc54226c4e47894cc6facd32133-446436058.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
